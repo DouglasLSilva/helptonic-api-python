82a2668305ed77b74901c4f9089af5ef92a9b655
Douglas Lopes <douglas_lopes.s@hotmail.com>
2021-04-25 22:59:48 -0300

    Tentando fazer essa merda funcionar na azure

---
+++ helptonic.py
@@ -0,0 +1,22 @@
+import flask
+from flask import send_file
+
+app = flask.Flask(__name__)
+app.config["DEBUG"] = True
+
+
+@app.route('/analyser', methods=['GET'])
+def imageAnalyser():
+    filename = 'images\\twittercard.png'
+    return send_file(filename, mimetype='image/png')
+
+@app.route('/health-check', methods=['GET'])
+def healthcheck():
+    status_code = flask.Response(status=200)
+    return status_code
+
+@app.route("/")
+def hello():
+    return "Hello Flask, on Azure App Service for Linux"
+
+app.run()
