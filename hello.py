0dec7f5330884b943742e8682071c4c018222c96
Douglas Lopes <douglas_lopes.s@hotmail.com>
2021-05-18 20:30:04 -0300

    Find Blue Color

--- hello.py
+++ hello.py
@@ -1,5 +1,11 @@
-from flask  import Flask, render_template, send_from_directory
+from flask  import Flask, render_template, send_from_directory, send_file, Response
+import numpy as np
+import cv2
 import pathlib
+from flask.helpers import make_response
+import io
+import imutils
+
 
 myapp = Flask(__name__, static_url_path="", static_folder="static")
 
@@ -9,7 +15,30 @@ def hello():
 
 @myapp.route("/analyser")
 def analyserImage():
-    return send_from_directory("static","colors.png");
+    imageOriginal = cv2.imread("static\\two_pencil.png")
+    image = cv2.cvtColor(imageOriginal, cv2.COLOR_BGR2HSV)
+    image = cv2.GaussianBlur(image, (5, 5), 0)
+
+    #Mascapa por Cor
+    lower_blue = np.array([97, 100, 117], dtype="uint8") #Azul
+    upper_blue = np.array([117, 255, 255], dtype="uint8") #Azul
+    mask = cv2.inRange(image, lower_blue, upper_blue)
+
+    #Buscando os contornos
+    cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL,
+    	cv2.CHAIN_APPROX_SIMPLE)
+
+    #Desenhando os contornos
+    cnts_imutils = imutils.grab_contours(cnts)
+    for c in cnts_imutils:
+        cv2.drawContours(imageOriginal, [c], -1, (0, 255, 0), 3)
+        M = cv2.moments(c)
+        cX = int(M["m10"] / M["m00"])
+        cY = int(M["m01"] / M["m00"])
+        cv2.putText(imageOriginal, "Cor Azul", (cX - 10, cY - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+        
+    data = cv2.imencode('.png', imageOriginal)[1].tobytes()
+    return Response(b'--frame\r\n' b'Content-Type: image/jpeg\r\n\r\n' + data + b'\r\n\r\n', mimetype='multipart/x-mixed-replace; boundary=frame')
 
 @myapp.errorhandler(Exception)
 def exception_handler(error):
